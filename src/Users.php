ea6b3845fa34e39cd2867c285aac2afd8a7a9d88
shovaintrocept <shova.shrestha@globaly.io>
2021-03-04 21:35:58 +1100

    composer file added

---
+++ src/Users.php
@@ -0,0 +1,29 @@
+<?php
+ require '../vendor/autoload.php';
+
+ class Users
+  {
+      function __construct()
+      {
+          
+      }
+
+      function validateEmail(){
+        echo "helloo shova";
+      }
+
+      function validateFileType(){
+
+      }
+
+      function validateName(){
+
+      }
+
+      function handleError(){
+
+      }
+ }
+ 
+
+?>
